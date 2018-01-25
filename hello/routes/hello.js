273a01ae2782b3079fdfd540a81ab17162d76f37
Jeremy Borja <jeremyborja@Jeremys-MacBook-Pro-7.local>
2018-01-25 10:58:36 -0800

    js

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,12 @@
  */
 
 exports.view = function(req, res){
+	var theWorld = req.params.userName;
+	if(theWorld === undefined){
+		theWorld = 'World';
+	}
+	console.log('Name is ' + theWorld);
   res.render('index', {
-  	'name': '',
+  	'name': theWorld,
   });
 };
